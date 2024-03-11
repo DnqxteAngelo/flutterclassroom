0eec58ed3dfa6eabf7f63be25f99c592ad92ef36
Angelo Serafino <serafino1412@gmail.com>
2024-03-11 14:16:23 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutterclassroom/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterclassroom
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Classroom',
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Classroom"),
+        centerTitle: true,
+        elevation: 5.0,
+        shadowColor: Colors.grey,
+        backgroundColor: Colors.black,
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 30.0,
+        ),
+      ),
+    );
+  }
+}
